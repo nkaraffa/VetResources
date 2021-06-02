444aabeec3ef42fd87c064c5dcce1a4e3d427797
nkaraffa <81177008+nkaraffa@users.noreply.github.com>
2021-06-02 18:42:21 -0400

    Add files via upload

---
+++ Models/Feedback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetResource.Models
+{
+    public partial class Feedback
+    {
+        public decimal FeedbackId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string FeedbackText { get; set; }    //Allows for multiline text
+    }
+}
