53e04fae09f5c4da38e4a27afa64aa3cea6195e0
nkaraffa <81177008+nkaraffa@users.noreply.github.com>
2021-06-17 19:44:27 -0400

    Add files via upload

---
+++ Controllers/FeedbackController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VetResource.Models;
+using VetResource.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Http;
+
+namespace VetResource.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // Variables that will allow for Feedback functionality
+        private IFeedback _feedback;
+        public FeedbackController (IFeedback feedback)
+        {
+            _feedback = feedback;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Feedback feedback)
+        {
+            Feedback newfeedback = _feedback.CreateFeedBack(feedback);
+            return RedirectToAction("ThankYou", "Feedback");
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Vsocont.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VetResource.Models
+{
+    public partial class Vsocont
+    {
+        public decimal VsoId { get; set; }
+        public string VsoName { get; set; }
+        public string VsoWebsite { get; set; }
+        public string VsoContact { get; set; }
+        public string VsoAddress { get; set; }
+    }
+}
